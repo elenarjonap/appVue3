be3c0a3aa9531b6828d98ac1cd46ec468ebe5e98
Elena <elenarjona@gmail.com>
2023-04-06 19:45:44 +0200

    VModel VFor and ComputedProps

--- src/views/MyView.vue
+++ src/views/MyView.vue
@@ -3,7 +3,11 @@ import SingleComponent from '../components/myComponents/SingleComponent.vue';
 import RawHtmlData from '../components/myComponents/RawHtmlData.vue';
 import DirectiveVShow from '../components/myComponents/directives/DirectiveVShow.vue';
 import DirectiveVIf from '../components/myComponents/directives/DirectiveVIf.vue';
-export default { components: { SingleComponent, RawHtmlData, DirectiveVShow, DirectiveVIf } };
+import DirectiveVModel from '../components/myComponents/directives/DirectiveVModel.vue';
+import DirectiveVFor from '../components/myComponents/directives/DirectiveVFor.vue';
+import ComputedProps from '../components/myComponents/computed-properties/ComputedProps.vue';
+
+export default { components: { SingleComponent, RawHtmlData, DirectiveVShow, DirectiveVIf, DirectiveVModel, DirectiveVFor, ComputedProps } };
 </script>
 
 <template>
@@ -18,6 +22,12 @@ export default { components: { SingleComponent, RawHtmlData, DirectiveVShow, Dir
     <DirectiveVShow/>
     --------------------
     <DirectiveVIf/>
+    --------------------
+    <DirectiveVModel/>
+    --------------------
+    <DirectiveVFor/>
+    --------------------
+    <ComputedProps/>
     
   </div>
 </template>
