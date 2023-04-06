ff18868cfe1114dd0c89f9d04d98ce22a0453fa9
Elena <elenarjona@gmail.com>
2023-04-06 21:18:50 +0200

    api Pokemon and api ChuckNorris

---
+++ src/components/myComponents/apiComponent/ApiChuckNorris.vue
@@ -0,0 +1,52 @@
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            chuckNorrisData:null,
+        }        
+    },
+    created(){
+        axios.get('https://api.chucknorris.io/jokes/random').then((result) => {
+            this.chuckNorrisData = result.data
+        })
+    }
+}
+</script>
+
+<template>
+    <div v-if="chuckNorrisData">
+        <h1>API CHUCK NORRIS</h1>
+        <div class="card chuckCard" style="width: 18rem;">
+            <h2>CHUCK NORRIS</h2>
+            <img src="https://api.chucknorris.io/img/chucknorris_logo_coloured_small@2x.png" class="card-img-top imgChuck" alt="Chuck Norris Api Logo">
+            <div class="card-body">
+                <p class="card-text">JOKE: {{ chuckNorrisData.value }}</p>
+                <p class="card-text">ID: {{ chuckNorrisData.id }}</p>
+            </div>
+        </div>
+    </div>        
+</template>
+
+<style>
+h1{
+    color: red;
+    text-align: center;
+}
+h2{
+  color: orangered;
+    text-align: center;
+}
+.chuckCard{
+    display: flex;
+    margin: auto;
+    background-color:aqua;
+    margin-top:10%;
+    text-align: left;
+    font-family: 'Courier New', Courier, monospace;
+}
+.imgChuck{
+    width:250px;
+    margin: auto;
+}
+</style>
---
+++ src/components/myComponents/apiComponent/ApiPokemon.vue
@@ -0,0 +1,49 @@
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            pokemonData:null,
+        }        
+    },
+    created(){
+        axios.get('https://pokeapi.co/api/v2/pokemon/pikachu').then((result) => {
+            this.pokemonData = result.data
+        })
+    }
+}
+</script>
+
+<template>
+    <div v-if="pokemonData">
+        <h1>API POKEMON</h1>
+
+        <div class="card pokemonCard" style="width: 18rem;">
+            <h5 class="card-title">Name: {{ pokemonData.name }}</h5>
+            <img class="imgPokemon card-img-top" :src="pokemonData.sprites.other.dream_world.front_default" alt='{{ pokemonData.name }}'>
+            <div class="card-body">
+                <p class="card-text">HP: {{ pokemonData.stats[0].base_stat }}</p>
+                <p>Speed: {{ pokemonData.stats[1].base_stat }}</p>
+                <p>Attack: {{ pokemonData.stats[3].base_stat }}</p>
+                <p>Weight: {{ pokemonData.weight }}</p>
+                <p>Height: {{ pokemonData.height }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.pokemonCard{
+    display: flex;
+    margin: auto;
+    margin-top:10%;
+    background-color: grey;
+    text-align: center;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: white;
+}
+.imgPokemon{
+    width:150px;
+    margin: auto;
+}
+</style>
